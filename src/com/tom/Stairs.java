344096f4d0989d3c63ba6c7bcf668b5bc78a2dc6
Hank Tom <hanktom@gmail.com>
2017-11-07 16:18:50 +0800

    Stairs

---
+++ src/com/tom/Stairs.java
@@ -0,0 +1,20 @@
+package com.tom;
+
+public class Stairs {
+	/*
+	 * 請使用者輸入一個數字，印出階梯型的星號
+	 * 如:  輸入6
+	 * 結果    *#####
+	 *     **####
+	 *     ***###
+	 *	   ****##
+	 *     *****#
+	 *     ****** 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+	}
+
+}
