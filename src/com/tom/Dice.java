1bb001e511642825dd7643683625e484efc2e81e
Hank Tom <hanktom@gmail.com>
2017-11-28 14:22:20 +0800

    Dice-1

---
+++ src/com/tom/Dice.java
@@ -0,0 +1,18 @@
+package com.tom;
+
+public class Dice {
+
+	public static void main(String[] args) {
+		int n1;
+		int n2;
+		int n3;
+		
+		int n[] = new int[3];
+		n[0] = 4;
+		n[1] = 5;
+		n[2] = 6;
+		for (int i=0; i<3; i++){
+			System.out.println(n[i]);
+		}
+	}
+}
