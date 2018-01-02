3c9798a313eac1ea623382c8073e97b34fd84a3e
Hank Tom <hanktom@gmail.com>
2018-01-02 15:16:13 +0800

    p8-8

--- src/com/tom/oo/GraduateStudent.java
+++ src/com/tom/oo/GraduateStudent.java
@@ -4,10 +4,12 @@ public class GraduateStudent extends Student{
 	int thesis;
 	public GraduateStudent(){
 		
+		System.out.println("haha");
 	}
 	
 	@Override
 	public void print(){
+		super.print();
 		System.out.println(pass);
 		System.out.println(english+"\t"+ 
 				math+"\t"+chinese +"\t" + thesis);
