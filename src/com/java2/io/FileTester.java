c50b77431d3a792c24216575dab98bc891efdb9c
Hank Tom <hanktom@gmail.com>
2018-04-10 16:24:30 +0800

    Java IO 101

---
+++ src/com/java2/io/FileTester.java
@@ -0,0 +1,25 @@
+package com.java2.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileTester {
+
+	public static void main(String[] args) {
+		try {
+			FileOutputStream out = new FileOutputStream("data.txt");
+			out.write(65);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
