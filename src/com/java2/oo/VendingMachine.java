1096229749a7f1db47eec51b768d2d8b53244ffe
Hank Tom <hanktom@gmail.com>
2018-06-05 14:53:39 +0800

    implement the method and testing

--- src/com/java2/oo/VendingMachine.java
+++ src/com/java2/oo/VendingMachine.java
@@ -2,7 +2,26 @@ package com.java2.oo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class VendingMachine {
+	int total = 0;
 	List<Drink> drinks = new ArrayList<>();
+	
+	public void on(){
+		System.out.println("Please insert coins...");
+		for (int i=0; i<drinks.size(); i++){
+			Drink drink = drinks.get(i);
+			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
+		}
+		Scanner scanner = new Scanner(System.in);
+		int data = -1;
+		while (data != 0){
+			data = scanner.nextInt();
+			switch(data){
+			case 0:
+				break;
+			}
+		}
+	}
 }
--- src/com/java2/oo/VendingMain.java
+++ src/com/java2/oo/VendingMain.java
@@ -1,10 +1,35 @@
 package com.java2.oo;
 
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class VendingMain {
 
 	public static void main(String[] args) {
+		VendingMachine vm = new VendingMachine();
+		FileReader fr;
+		try {
+			fr = new FileReader("vending.txt");
+			BufferedReader in = new BufferedReader(fr);
+			String line = in.readLine();
+			String[] tokens = line.split(",");
+			int count = Integer.parseInt(tokens[0]);
+			for (int i = 0; i < count; i++){
+				String name = tokens[i*2+1];
+				int price = Integer.parseInt(tokens[i*2+2]);
+				vm.drinks.add(new Drink(i+1, name, price));
+			}
+			vm.on();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
