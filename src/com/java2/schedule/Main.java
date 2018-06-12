e1b57364606224ed034c87872283d08ac2c12924
Hank Tom <hanktom@gmail.com>
2018-06-12 15:59:53 +0800

    Course Schedule practice

---
+++ src/com/java2/schedule/Main.java
@@ -0,0 +1,20 @@
+package com.java2.schedule;
+
+/* 有一文字檔儲存了課表資料(schedule.txt)
+ * 請讀取資料後，設計程式可檢查是否有空檔?
+ * 如:
+ * 請輸入星期(1-7):2
+ * 請輸入時(0-24):11
+ * 【有空檔】
+ * 請輸入星期(1-7):4
+ * 請輸入時(0-24):14
+ * 【有課: C004 English 到 16時 】
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
